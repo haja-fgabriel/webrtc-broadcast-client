2a647c6bc98a8e566de598c667422e52d332b5f0
Haja Florin-Gabriel <haja.fgabriel@gmail.com>
2021-05-30 17:08:40 +0300

    Completed test for RTCForwardingPeer that is watching for new sons

--- test/rtc/core/RTCForwardingPeer.js
+++ test/rtc/core/RTCForwardingPeer.js
@@ -1,15 +1,20 @@
 import { RTCForwardingPeer } from '../../../src/rtc/core/RTCForwardingPeer'
 
 let peer
+let otherPeer
+let roomName
 
 beforeAll(function () {
   peer = new RTCForwardingPeer({
     serverUrl: 'http://localhost:8000'
   })
+  otherPeer = new RTCForwardingPeer({
+    serverUrl: 'http://localhost:8000'
+  })
+  roomName = 'default-room-test-' + Math.round(Math.random() * 1000)
 })
 
-// eslint-disable-next-line jest/no-done-callback
-test('Connect', () => {
+async function testConnect (peer) {
   return new Promise(function (resolve, reject) {
     peer.onConnectionStateChange = function () {
       console.log(peer.connectionState)
@@ -20,13 +25,32 @@ test('Connect', () => {
     }
     peer.connectToServer()
   })
+}
+
+test('Connect', () => {
+  return expect(testConnect(peer)).resolves.toBe(undefined)
+})
+
+test('Connect the other', () => {
+  return expect(testConnect(otherPeer)).resolves.toBe(undefined)
 })
 
 test('Join room', () => {
-  return expect(peer.joinRoom('default-room-test')).resolves.toBe('viewer')
+  return expect(peer.joinRoom(roomName)).resolves.toBe('broadcaster')
+})
+
+test('The other joins the room', () => {
+  return new Promise((resolve, reject) => {
+    otherPeer.joinRoom(roomName).then((as) => {
+      expect(as).toBe('viewer')
+      resolve()
+    })
+  })
 })
 
 afterAll(function () {
+  otherPeer.removeAllListeners()
+  otherPeer.close()
   peer.removeAllListeners()
   peer.close()
 })
